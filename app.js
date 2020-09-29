cb6abaebbf551dbef0ec94b6759f0abc901cf02e
sarah-svg <kjskillen@gmail.com>
2020-09-29 14:38:18 -0700

    working progess
    kkk

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-// import functions and grab DOM elements
 
-// initialize state
+    // random value generated 
+let y = Math.floor(Math.random() * 20 + 1); 
+      
+    // counting the number of guesses 
+    // made for correct Guess 
+let number = 1; 
+      
+document.getElementById('submitguess').onclick = function(){ 
+      
+   // number guessed by user      
+    let x = document.getElementById('guessField').value; 
+  
+    if (x === y) 
+    {     
+        alert(`CONGRATULATIONS!!! ${number} GUESS `); 
+    } 
+    else if (x > y)  
+    {     
+        number++; 
+        alert('too high'); 
+    } 
+    else
+    { 
+        number++; 
+        alert('too low'); 
+    } 
+}; 
 
-// set event listeners to update state and DOM
